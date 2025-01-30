67799d95ff916d807b3f99cb6bf55e0060b58d3d
erikscar <erikscarcela@gmail.com>
2025-01-30 13:08:38 -0300

    Config

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import http from 'http'
 import cors from 'cors';
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite';
-const port = process.env.PORT || 5000;
+const port = 3000;
 
 const db = await open({
-    filename: process.env.DATABASE_URL || 'chat.db',
+    filename: 'chat.db',
     driver: sqlite3.Database
 })
 
